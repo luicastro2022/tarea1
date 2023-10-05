7a838cd803c376258bbf23b3d23a211a8ec3a84d
vicente132 <vihernandez2022@udec.cl>
2023-10-05 11:12:47 -0300

    metodos de pago
    clase Pago y sus hijos

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class OrdenCompra{
     private Date fecha;
     private String estado;
 
-    public void OrdenCompra(){}
+    public OrdenCompra(){}
 
     public int calcPrecioSinIVA(){
         return 2;
@@ -14,9 +14,29 @@ class OrdenCompra{
     public int calcPrecio(){return 2;}
     public int calcPeso(){return 2;}
 
+
 }
 
+class Pago{
+    private float monto;
+    private Date fecha;
 
+    public Pago(){}
+}
+class Efectivo extends Pago{
+    public Efectivo(){}
+    public float calcDevolucion(){return 0;}
+}
+class Transferencia extends Pago{
+    private String banco;
+    private String numCuenta;
+
+}
+class Tarjeta extends Pago{
+    private String tipo;
+    private String numTransaccion;
+
+}
 
 
 
